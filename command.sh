005dc9c6972e0183b80583d2578d2096fe1025f4
abdou442 <111877577+abdou442@users.noreply.github.com>
2025-09-27 16:18:49 +0100

    Add files via upload

---
+++ command.sh
@@ -0,0 +1,9 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install curl -y
+sudo apt install xorg -y
+sudo apt install i3 -y 
+cd ~
+sudo apt install build-essential python3 perl default-jdk -y
+sudo apt install greetd
+sudo apt install greetd-gtkgreet
