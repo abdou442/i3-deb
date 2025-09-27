b53472ad3db3120ae8544b77f053602d84f96e75
abdou442 <111877577+abdou442@users.noreply.github.com>
2025-09-27 01:49:42 +0100

    command.sh

--- command.sh
+++ command.sh
@@ -4,12 +4,14 @@ sudo apt install curl -y
 sudo apt install xorg -y
 sudo apt install i3 -y 
 cd ~
-wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz
-tar xf zig-linux-x86_64-0.13.0.tar.xz
-echo 'export PATH="$HOME/zig-linux-x86_64-0.14.1:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+sudo apt install build-essential python3 perl default-jdk -y
+curl -sS https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/debian.griffo.io.gpg
+echo "deb https://debian.griffo.io/apt $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/debian.griffo.io.list
+sudo apt update
+sudo apt install zig
 sudo apt install build-essential libpam0g-dev libxcb-xkb-dev xauth xserver-xorg brightnessctl -y
 git clone https://codeberg.org/fairyglade/ly.git
-cd ly
+cd ~/ly
 make
-
+make install 
+sudo systemctl enable ly
